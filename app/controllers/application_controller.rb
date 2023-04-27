b973c40930538e5fd1fc375e143cdccd8d15e46d
mihasi <mihasiakihiro0818@gmail.com>
2023-04-27 18:49:00 +0900

    投稿リンクの変更

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,14 +1,15 @@
 class ApplicationController < ActionController::Base
-    require 'pagy'
-    include Pagy::Backend
-    include SessionsHelper
-    helper_method :current_user
-    
-    protect_from_forgery with: :exception
-    
-    private
-    
-    def current_user
-      @current_user ||= User.find(session[:user_id]) if session[:user_id]
-    end
+  require 'pagy'
+  include Pagy::Backend
+  include SessionsHelper
+  helper_method :current_user
+  
+  protect_from_forgery with: :exception
+  
+  private
+  
+  def current_user
+    @current_user ||= User.find(session[:user_id]) if session[:user_id]
+  end
+
   end
